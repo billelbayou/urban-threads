9ea37f6cc869fcb1dc9f42e1cf7b5dc93c940ca1
Bayou Billel <bayoubillel2004@gmail.com>
2026-02-01 00:02:09 +0100

    fix

--- backend/src/utils/prisma.ts
+++ backend/src/utils/prisma.ts
@@ -1,11 +1,10 @@
-import { Pool } from "pg";
+import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../generated/prisma/client.js";
+import { PrismaClient } from "../generated/prisma/client";
 
 const connectionString = `${process.env.DATABASE_URL}`;
 
-const pool = new Pool({ connectionString });
-const adapter = new PrismaPg(pool);
+const adapter = new PrismaPg({ connectionString });
 const prisma = new PrismaClient({ adapter });
 
 export { prisma };
